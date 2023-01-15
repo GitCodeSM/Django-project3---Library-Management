848f71d7b39e34f7e74774a215421a22b0076320
GitCodeSM <105416546+GitCodeSM@users.noreply.github.com>
2023-01-15 23:35:09 +0530

    Add files via upload

--- management/models.py
+++ management/models.py
@@ -6,13 +6,17 @@ class Member(models.Model):
 
     member_name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.member_name
+
 class Book(models.Model):
     book_title = models.CharField(max_length=150)
     author = models.CharField(max_length=100)
     book_cover = models.ImageField(upload_to='static/')
     description = models.TextField()
     status = models.CharField(default="Available", max_length=10)
-    member_name = models.ForeignKey(Member, on_delete=models.CASCADE, primary_key=True)
+    member_name = models.ForeignKey(Member, on_delete=models.CASCADE, related_name = 'mybooks')#, primary_key=True)
 
     def __str__(self):
 	    return self.book_title
+# default = option 1 = id field, value = None
--- management/urls.py
+++ management/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path
+# from django.urls import path
 
-from management import views
+# from management import views
 
-urlpatterns = [
-    path('', views.homepage, name = "homepage"),
-    path('signup/', views.signup, name = "signup"),
-    path('login/', views.login, name = 'login'),
-    path('logout/', views.logout, name = 'logout'),
-]
+# urlpatterns = [
+#     path('', views.homepage, name = "homepage"),
+#     path('signup/', views.signup, name = "signup"),
+#     path('login/', views.login, name = 'login'),
+#     path('logout/', views.logout, name = 'logout'),
+# ]
